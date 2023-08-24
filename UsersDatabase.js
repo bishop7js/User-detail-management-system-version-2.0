ffc939d3f1d5f741e6c2b3cf06714b0ce1bb3458
bishop7js <sulakshithait@gmail.com>
2023-08-24 09:47:24 +0530

    Add users data and show them inside data.json file

--- UsersDatabase.js
+++ UsersDatabase.js
@@ -1,24 +1,58 @@
-//const chalk = require("chalk");
+const fs = require('fs');
 
-const addUser = () => {
-  console.log("Add user");
+const DatabaseFile = 'data.json';
+
+const addUser = (value) => {
+  let users = loadUsers();
+
+  usersData = {
+    name: value.name,
+    address: value.address,
+    contact_number: value.contact_number,
+    visit_date: value.visit_date
+  }
+
+  users.push(usersData);
+
+  saveUser(users);
 };
 
-const updateUser = () => {
-  console.log("Update user")
+const updateUser = (id) => {
+  console.log("Update user", id)
 }
 
-const deleteUser = () => {
-  console.log("Delete user")
+const deleteUser = (id) => {
+  console.log("Delete user", id)
 }
 
-const readUser = () => {
-  console.log("Read user")
+const readUser = (id) => {
+  console.log("Read user", id)
 }
+
+const userList = () => {
+  console.log("User list")
+}
+
+// Save user inside the file
+const saveUser = (users) => {
+  fs.writeFileSync(DatabaseFile, JSON.stringify(users))
+}
+
+//load data from file
+const loadUsers = () => {
+  const dataBuffer = fs.readFileSync(DatabaseFile);
+  const dataJson = dataBuffer.toString();
+  return JSON.parse(dataJson)
+}
+
+console.log('LLLLLLLLLLload users', loadUsers);
+
+
 
 module.exports = {
   addUser,
   updateUser,
   deleteUser,
-  readUser
+  readUser,
+  userList
 };
--- index.js
+++ index.js
@@ -1,26 +1,94 @@
-const yargs = require('yargs');
-const database = require('./UsersDatabase');
+const yargs = require("yargs");
+const database = require("./UsersDatabase");
 
+//node index.js add --name="Sulakshitha" --address="Colombo" --contact_number=1234 --visit_date="2023/08/23"
 
-
+// Add a user
 yargs.command({
-    command: "add",
-    describe: "To add a player",
-    builder: {
-        name: {
-            describe: "Name",
-            demandOption: true,
-            type: "string"
-        },
-        address: {
-            describe: "Adress",
-            demandOption: true,
-            type: "string"
-        },
+  command: "add",
+  describe: "To add a player",
+  builder: {
+    name: {
+      describe: "Name",
+      demandOption: true,
+      type: "string",
     },
-    handler: function() {
-        database.addUser()
-    }
-})
+    address: {
+      describe: "Adress",
+      demandOption: true,
+      type: "string",
+    },
+    contact_number: {
+      describe: "Adress",
+      demandOption: true,
+      type: "string",
+    },
+    visit_date: {
+      describe: "Adress",
+      demandOption: true,
+      type: "string",
+    },
+  },
+  handler(argv) {
+    database.addUser(argv);
+  },
+});
+
+// Update a user
+yargs.command({
+  command: "update",
+  describe: "To update a player",
+  builder: {
+    id: {
+      describe: "Id",
+      demandOption: true,
+      type: "number",
+    },
+  },
+  handler(argv) {
+    database.updateUser(argv.id);
+  },
+});
+
+// Delete a user
+yargs.command({
+  command: "delete",
+  describe: "To delete a player",
+  builder: {
+    id: {
+      describe: "Id",
+      demandOption: true,
+      type: "number",
+    },
+  },
+  handler(argv) {
+    database.deleteUser(argv.id);
+  },
+});
+
+// Read a user
+yargs.command({
+  command: "read",
+  describe: "To Read a player",
+  builder: {
+    id: {
+      describe: "Id",
+      demandOption: true,
+      type: "number",
+    },
+  },
+  handler(argv) {
+    database.readUser(argv.id);
+  },
+});
+
+// Users list
+yargs.command({
+    command: "list",
+    describe: "To list players",
+    handler() {
+      database.userList();
+    },
+  });
 
 yargs.parse();
